8d37289f5e2a79e3eb9735afa827ab174283677b
Jsabs <sabillano057@gmail.com>
2025-08-15 11:10:25 +0800

    Aug 15

--- Switch/script.js
+++ Switch/script.js
@@ -1,20 +1,20 @@
     let day = prompt("What's Day Today: ");
 
     switch(day) {
-        case'Monday':
-            console.log("The day today is Friday");
+        case 'Monday':
+            console.log("Yes you're correct today is Monday");
             break;
 
         case 'Tuesday':
-            console.log("The day today is Friday");
+            console.log("Yes you're correct today is Tuesday");
             break;
 
         case 'Wednesday':
-            console.log("The day today is Friday");
+            console.log("Yes you're correct today is Wednesday");
             break;
 
         case 'Thursday':
-            console.log("The day today is Friday");
+            console.log("Yes you're correct today is Thursday");
             break;
 
         case 'Friday':
